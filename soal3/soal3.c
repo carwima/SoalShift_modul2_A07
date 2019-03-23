78fea03971e5b59906bde77ed5140fc623ef9147
carwima <36812643+carwima@users.noreply.github.com>
2019-03-24 03:00:46 +0700

    Add files via upload

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -8,60 +8,29 @@
 
 int main(){
     pid_t cid, cid1, cid2;
-    int line[2], status;
-    cid=fork();
-    pipe(line);//pip pertama
-    pipe(line+2);//pip kedua
-    if (cid<0) {
-        exit(EXIT_FAILURE);
-    }
-    else if (cid==0) {
-            char *zip[] = {"unzip", "campur2.zip", NULL};
-            execv(zip[0], zip); 
-    }
-    
-    else
-    {
-        while(wait(&status)>0);
-        cid1=fork();
-        if (cid==0) {
-            dup2(line[1],1);//buat menyimpan final ls nanti. Intinya buat write
-            close(line[1]);
-            close(line[0]);
-            close(line[3]);
-            close(line[2]);
-            char *look={"ls","/home/honesta/Documents/sisop/SoalShift/modul2/soal3/campur2/", NULL};
-            execv(look[0],look);
+    int line[2],tes[2], status2, status3;
+    pipe(line);
+	if((cid1=fork())==0){
+    		char *zip[] = {"unzip", "campur2.zip", NULL};
+		execvp(zip[0], zip);
+	}
+	else{
+		while ((wait(&status2)) > 0);
+		if((cid2=fork())==0){
+			dup2(line[1],1);
+			close(line[0]);
+                        close(line[1]);
+			char *list[] = {"ls","/home/carwima/SoalShift_modul2_A07/soal3/campur2/", NULL};
+        		execvp(list[0], list);
+		}
+		else{
+			while((wait(&status3))>0);
+			dup2(line[0],0);
+			freopen ("daftar.txt","w",stdout);
+			close(line[1]);
+			char *grab[] = {"grep", ".txt$", NULL};
+			execvp(grab[0], grab);
+		}
         }
-        else
-        {
-            cid2=fork();
-            if (cid2>0) {
-                dup2(line[0],0);//ngebaca yang dari ls tadi
-                dup2(line[3],1);//nyimpan final dari greb nanti
-                close(line[3]);
-                close(line[1]);
-                close(line[2]);
-                close(line[0]);
-                char *g[]={"grep",".txt$",NULL};
-                execvp(g[0],g);//ketika ada file yang paling belakang isinya.txt diambil
-                //printf("hell");       
-            }
-            else
-            {
-                //wait(NULL);
-                char final[1000];
-                close(line[1]);
-                close(line[0]);
-                close(line[3]);
-                read(line[2],final,sizeof(final));//ngebaca dari greb tadi
-                close(line[2]);
-                //printf("%s",final);
-                FILE *file=fopen("/home/honesta/Documents/sisop/SoalShift/modul2/soal3/daftar.txt","w+");//buat file
-                fputs(final,file);//memasukkan final ke file
-                fclose(file);          
-                //printf("%d",check);
-            }
-        }      
-    }
+
 }
